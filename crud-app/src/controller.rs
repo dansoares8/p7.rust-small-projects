42b5e410522c677003bc0ba4ffddfa67b0909f20
dansoares8 <danielsoares8296@gmail.com>
2024-12-28 03:16:20 -0300

    up

--- crud-app/src/controller.rs
+++ crud-app/src/controller.rs
@@ -7,4 +7,15 @@ pub async fn list_users() -> (StatusCode, Json<Value>){
     //get users
 }
 
-pub async fn get_user_by_id(Path<u64>)
+pub async fn get_user_by_id(Path(id):Path<u64>) -> (StatusCode, Json<Value>){
+
+}
+
+pub async fn create_user(Json(user) :Json<User>) -> StatusCode{
+
+}
+
+pub async fn update_user(Path(id):Path<u64>, Json(user) :Json<User>)
+    -> StatusCode{
+
+}
